dbfddb16013d24cd0a34464970135d19dcc1fe00
Hide <padocon@naver.com>
2020-01-13 21:05:34 +0900

    Update notice

--- src/components/About.vue
+++ src/components/About.vue
@@ -7,6 +7,12 @@
       Codelog는 일상속에서 스쳐 지나가는 개발 관련 URL들을 한곳에 집중시키기 위해 제작된 사이트입니다. 
     </div>
     <div class="about-header">
+      <p>Notice</p>
+    </div>
+    <div class="about-body">
+      본 사이트는 유저의 Github 이메일, 닉네임을 수집합니다. Github를 통해 로그인을 하지만 이메일, 닉네임을 제외한 기타 정보들은 저장하지 않습니다.<br />
+    </div>    
+    <div class="about-header">
       <p>Rules</p>
     </div>
     <div class="about-body">
